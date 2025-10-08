078792df26647ad3fa760c9de8783157e05e94fb
AnanthNaveen <naveenananth.sbjh@gmail.com>
2025-10-08 11:53:38 +0530

    changes in dummy file

--- resources/views/doctpanel/spine_pages/dummy_spine.blade.php
+++ resources/views/doctpanel/spine_pages/dummy_spine.blade.php
@@ -225,6 +225,7 @@
                             <label for="">N</label>
                         </div>
                         <input type="text" placeholder="Specify Trigger">
+                        <input type="text" placeholder="Specify Trigger">
                     </div>
                 </div>
             </div>
